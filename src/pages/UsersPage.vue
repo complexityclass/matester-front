f924db4a3a9cd34052b012a9d3dbd81caea5e0d4
anastasiya <just-fy@mail.ru>
2021-02-12 10:51:57 +0300

    fix users list

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -55,6 +55,9 @@ export default {
   computed: {
     basicAuth() {
       return localStorage.getItem('matesterBasicAuth');
+    },
+    userLogin() {
+      return localStorage.getItem('matesterLogin');
     }
   },
   created() {
@@ -67,7 +70,11 @@ export default {
       }).then(usersResponse => {
         console.log('usersResponse', usersResponse)
         if (usersResponse.data && usersResponse.data.length !== 0) {
-          this.users = usersResponse.data;
+          usersResponse.data.forEach(el => {
+            if (el.login !== this.userLogin) {
+              this.users.push(el);
+            }
+          })
           axios.get(`https://matester23.herokuapp.com/friends`, {
             headers: { 'Authorization': this.basicAuth }
           }).then(friendsResponse => {
